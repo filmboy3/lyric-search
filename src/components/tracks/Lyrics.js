97e0989b692fcaf1be756301f2fb855db234bd79
filmboy3 <jonathanschwartz30@gmail.com>
2019-02-10 20:37:50 -0500

    added nested fetch for extra lyric data

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,6 +1,39 @@
 import React, { Component } from "react";
 
 class Lyrics extends Component {
+  state = {
+    track: {},
+    lyrics: {},
+    id: ""
+  };
+
+  componentDidMount() {
+    fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${
+        this.props.match.params.id
+      }&apikey=${process.env.REACT_APP_MM_KEY}`
+    )
+      .then(res => res.json())
+      .catch(error => console.error("Error:", error))
+      .then(({ message }) => {
+        let lyrics = message.body.lyrics;
+        this.setState({
+          lyrics: lyrics
+        });
+        return fetch(
+          `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${
+            this.props.match.params.id
+          }&apikey=${process.env.REACT_APP_MM_KEY}`
+        );
+      })
+      .then(res => res.json())
+      .catch(error => console.error("Error:", error))
+      .then(({ message }) => {
+        let data = message.body.track;
+        console.log(data);
+      });
+  }
+
   render() {
     return (
       <div>
