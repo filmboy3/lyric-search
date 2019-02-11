fea3962017dbcac1d92a94ed02e7706aad7de15b
filmboy3 <jonathanschwartz30@gmail.com>
2019-02-10 22:56:51 -0500

    Updates

---
+++ precache-manifest.36b0bb05376bd683e3e93fbd68a05df9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a74a3a62060861c4d9d",
+    "url": "/lyric-search/static/css/main.bff4bc7f.chunk.css"
+  },
+  {
+    "revision": "5a74a3a62060861c4d9d",
+    "url": "/lyric-search/static/js/main.5a74a3a6.chunk.js"
+  },
+  {
+    "revision": "e71e4ed955d4385c9e98",
+    "url": "/lyric-search/static/js/1.e71e4ed9.chunk.js"
+  },
+  {
+    "revision": "064d659a8875b21723da",
+    "url": "/lyric-search/static/js/runtime~main.064d659a.js"
+  },
+  {
+    "revision": "76ae8aa6d5703f35f3169a5093567b15",
+    "url": "/lyric-search/index.html"
+  }
+];
